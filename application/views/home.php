cf1757b7d53df61f5be16d2262408ebf4a8486dc
edavis25 <davis.25811@gmail.com>
2017-01-25 18:07:27 -0500

    Homepage adjustments

--- application/views/home.php
+++ application/views/home.php
@@ -18,26 +18,44 @@
 <!-- /.row -->
 
 <div class="row">
+    
+    
+    
     <div class="container col-xs-4">
         <div class="panel">
             <div class="panel-heading">
-                Panel 1 Header
+                <i class="fa fa-info" aria-hidden="true"></i> <b>About the Project</b>
             </div>
             <div class="panel-body">
-                Panel 1
+                <p>
+                	The Saberskeptics relational database contains nearly 150,000 rows of detailed NFL statistics from the past 10 years. Harvested using custom-built Selenium web scrapers, the database contains
+                	complete player and team statistics organized by game performance. 
+                </p>
+                <br />
+                <ul>
+                	<li>For more info on the project, check out the <a href="">about page</a></li>
+					<li>For more info on the database, check out the <a href="">schema</a></li>
+                </ul>
+                <a href="https://github.com/edavis25/Fantasy-Football-Database"><i class="fa fa-github fa-2x"></i> All your stars are belong to us!</a>
+                <br />
+                <br />
+                <small><b>Disclaimer:</b> The Saberskeptics project is purely nonprofit and for educational purposes only! (Please don't sue us Mr. Goodell)</small>
             </div>
         </div>
     </div>
     <div class="container col-xs-4">
         <div class="panel">
             <div class="panel-heading">
-                Panel 2 Header
+                <i class="fa fa-fw fa-dashboard"></i> <b>Dynamic Team Dashboards</b>
             </div>
             <div class="panel-body">
-                Panel 2
+                <p>Dynamically generated, interactive dashboards to visualize your favorite team's season performance!</p>
+                <img src="img/dashboard-500.png" class="img img-responsive" style="height: 210px; margin-left: auto; margin-right: auto; "/>
             </div>
         </div>
     </div>
+    
+    
     <div class="container col-xs-4">
         <div class="panel">
             <div class="panel-heading">
