158647aa879eb705f7168cdc569a40f78716781e
edavis25 <davis.25811@gmail.com>
2017-02-26 22:17:16 -0500

    Started homepage carousel.

--- application/views/home.php
+++ application/views/home.php
@@ -12,12 +12,48 @@
 <div class="row">
     <div class="jumbotron">
         <h1 class="page-header"> <b><i>Saber<span style="color:#0F0064;">skeptics</span></i></b></h1>
-        <p id="site-subtitle" class="text-muted">10 years of NFL player, team, and game statistics.</p>
+        <p id="site-subtitle" class="text-muted">10 years of NFL statistics, analysis, visualizations, &amp; projections.</p>
     </div>
 </div>
 <!-- /.row -->
 
 <div class="row">
+    
+    
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		<!-- Slide Number Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1"></li>
+		</ol>
+
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner" role="listbox">
+
+			<!-- Slide 1 -->
+			<div class="item active">
+				<!-- h3 style="text-align: center;">Dynamic Team Dashboards</h3 -->
+				<!-- h1 style="position: absolute; background-color: rgba(0,0,0,0.8); color: white; margin-left: 20%; float: bottom;">Team Dashboards</h1 -->
+				<img src="img/dashboard-wide1.png" alt="" style="height: 470px; margin-left: auto; margin-right: auto;" />
+				<div style="position: absolute; width: 75%; left: 12.5%; background-color: rgba(10,0,68, 1); color: white; bottom: 30px;">
+					<h3 style="text-align: center"><b>Team Dashboards</b></h3>
+					<div style="color: white; text-align: center;">
+						&bull; Detailed Season Rankings &amp; Statistics<br />
+						&bull; Dynamically Generated Interactive Visualizations &amp; Charts<br /><br />
+					</div>
+				</div>
+			</div>
+			<!-- Slide 2 -->
+			<div class="item">
+				<img src="img/dashboard-500.png" alt=""/>
+			</div>
+		</div>
+
+		<!-- Left and right controls -->
+		<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"> <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a>
+		<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"> <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> <span class="sr-only">Next</span> </a>
+	</div>
+
     
     
     
